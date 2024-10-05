aaaae4d7e6ddc6bff09ca2242348b7c4309e84cd
Riza Fahmi <rizafahmi@gmail.com>
2024-10-05 12:31:54 +0700

    Fix feed rss. WIP

--- lib/podcast_feed_web/controllers/home_controller.ex
+++ lib/podcast_feed_web/controllers/home_controller.ex
@@ -18,33 +18,30 @@ defmodule PodcastFeedWeb.HomeController do
           <title>
           <![CDATA[Ngobrolin WEB]]>
           </title>
-          <description>
-          <![CDATA[Yuk mari kita diskusi dan ngobrol ngalor-ngidul tentang dunia web. Agar tetap up-to-date dengan teknologi web terkini.
+          <description><![CDATA[Yuk mari kita diskusi dan ngobrol ngalor-ngidul tentang dunia web. Agar tetap up-to-date dengan teknologi web terkini.
 
       Topik, tautan dan pertanyaan menarik bisa dilayangkan ke https://bit.ly/ngobrolinweb]]>
           </description>
           <link>https://ngobrol.in/web</link>
           <image>
-          <url><![CDATA[https://ngweb-assets.s3.amazonaws.com/thumbnail-podcast-square.png]></url>
+          <url><![CDATA[https://ngweb-assets.s3.amazonaws.com/thumbnail-podcast-square.png]]></url>
           <title>Ngobrolin WEB</title>
           <link>https://ngobrol.in/web</link>
           </image>
           <generator>Podcast Creator</generator>
           <lastBuildDate>Tue, 24 Sep 2024 10:27:25 GMT</lastBuildDate>
-          <atom:link href="https://anchor.fm/s/30348d4/podcast/rss" rel="self" type="application/rss+xml"/>
-          <author><![CDATA[Ngobrolin WEB]]></author>
+          <atom:link href="https://ngobrolinweb.riza.world/feed" rel="self" type="application/rss+xml"/>
+          <author>Ngobrolin WEB</author>
           <copyright>
           <![CDATA[Ngobrolin WEB]]>
           </copyright>
           <language>
           <![CDATA[in]]>
           </language>
-          <atom:link rel="self" />
           <itunes:author>Ngobrolin WEB</itunes:author>
-          <itunes:summary>
-          Yuk mari kita diskusi dan ngobrol ngalor-ngidul tentang dunia web. Agar tetap up-to-date dengan teknologi web terkini.
+          <itunes:summary><![CDATA[Yuk mari kita diskusi dan ngobrol ngalor-ngidul tentang dunia web. Agar tetap up-to-date dengan teknologi web terkini.
 
-          Topik, tautan dan pertanyaan menarik bisa dilayangkan ke https://bit.ly/ngobrolinweb
+          Topik, tautan dan pertanyaan menarik bisa dilayangkan ke https://bit.ly/ngobrolinweb]]>
           </itunes:summary>
           <itunes:type>episodic</itunes:type>
           <itunes:owner>
@@ -66,7 +63,6 @@ defmodule PodcastFeedWeb.HomeController do
               <link>https://ngweb-assets.s3.amazonaws.com/Ngobrolin%20URL%20-%20Ngobrolin%20WEB%20ep45%20%5BEMynH33TrBI%5D.mp4</link>
               <pubDate><%= episode.updated_at %></pubDate>
               <guid isPermaLink="false"><%= episode.id %></guid>
-              <dc:creator>Ngobrolin WEB</dc:creator>
               <enclosure url="<%= episode.link %>" length="<%= episode.duration %>" type="audio/mpeg" />
               <itunes:summary>
               <![CDATA[<%= episode.description %>]]>
@@ -85,7 +81,8 @@ defmodule PodcastFeedWeb.HomeController do
       |> EEx.eval_string(assigns: [episodes: episodes])
 
     conn
-    |> put_resp_content_type("application/rss+xml")
+    # |> put_resp_content_type("application/rss+xml")
+    |> put_resp_content_type("application/xml")
     |> send_resp(200, xml_data)
   end
 end
